34788e777bbb5efd3e92b70d7e3da60077f90346
Xiong Ben <xiongben1993@gmail.com>
2019-09-28 22:20:18 +0800

    add

--- hello.c
+++ hello.c
@@ -20,24 +20,46 @@
    
 // }
 
+// int main ()
+// {
+//    int  var;
+//    int  *ptr;
+//    int  **pptr;
+
+//    var = 3000;
+
+//    /* 获取 var 的地址 */
+//    ptr = &var;
+
+//    /* 使用运算符 & 获取 ptr 的地址 */
+//    pptr = &ptr;
+
+//    /* 使用 pptr 获取值 */
+//    printf("Value of var = %d\n", var );
+//    printf("Value available at *ptr = %d\n", *ptr );
+//    printf("Value available at **pptr = %d\n", **pptr);
+
+//    return 0;
+// } 
+
 int main ()
 {
-   int  var;
-   int  *ptr;
-   int  **pptr;
-
-   var = 3000;
-
-   /* 获取 var 的地址 */
-   ptr = &var;
-
-   /* 使用运算符 & 获取 ptr 的地址 */
-   pptr = &ptr;
-
-   /* 使用 pptr 获取值 */
-   printf("Value of var = %d\n", var );
-   printf("Value available at *ptr = %d\n", *ptr );
-   printf("Value available at **pptr = %d\n", **pptr);
-
-   return 0;
+    // int xb = 20;
+    // int *a ;
+    // a = &xb;
+    // int **b = &a;
+    // printf("a=%d\n",a);
+    // printf("*a=%d\n",*a);
+    // printf("**b=%d\n",**b);
+    // printf("b=%d\n",b);
+    // int array[10]={0,1,2,3,4,5,6,7,8,9},value;
+    // value=*(array+3);
+    // printf("value=%d\n",value);
+    int xb = 20;
+    int *a = &xb;
+    // int *a = xb;
+   
+    printf("a=%d\n",a);
+    printf("*a=%d\n",*a);
+   
 }
